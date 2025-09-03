64dbae0c2b2053c305c674f616575e5652da09af
Norahmw21 <norahmw2@gmail.com>
2025-09-03 12:21:37 +0300

    edits

--- src/main/java/com/wakeb/Calculator.java
+++ src/main/java/com/wakeb/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
     public static int add(int a, int b) {
         return a + b;
     }
-
+//hello
     public static int subtract(int a, int b) {
         return a - b;
     }
